9703fbf2ac2637780086a21f0553c936d16c6d4a
tmahere <takundamahere.tm@gmail.com>
2023-10-11 19:52:35 -0400

    installed pylint, ran code to ensure functionality

---
+++ src/python_package/lint.py
@@ -0,0 +1,20 @@
+#lint.py
+
+import sys
+
+from pylint import lint
+
+THRESHOLD = 9
+
+run = lint.Run(["hello_world.py"], do_exit=False)
+
+score = run.linter.stats.global_note
+
+if score < THRESHOLD:
+
+    print("Linter failed: Score < threshold value")
+
+    sys.exit(1)
+
+
+sys.exit(0)
